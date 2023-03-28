fc2abcecaf0e5d10b92b40bb4f6524095f26d0de
AdanYanes <126865927+AdanYanes@users.noreply.github.com>
2023-03-28 10:43:09 +0100

    feed(Main): added user number imput to sout

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hola");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        int number = Integer.parseInt(scanner.nextLine());
+        System.out.println("Hola Mundo " + number + "!");
+        scanner.close();
         
     }
 }
